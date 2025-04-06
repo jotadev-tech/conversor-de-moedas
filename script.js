5e40f951df7d2f048856857f31e249fe0f8f5629
jotadev-tech <joelsonleite177@gmail.com>
2025-04-06 19:07:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function conversor(){
+    let valor = prompt("Digite o valor em Reais para ser convertido para Dolar: ");
+    let valorConvertido = valor * 5.75;
+    alert(`O valor de ${valor} reais convertido para dolar é  ${valorConvertido} dolares`);
+}
+
+
+ 
+
+
+
+
+
